7a7315e15f29dbfd4b15c27073790c15e5c70ce1
Laurent <lrspam@sfr.fr>
2026-02-07 16:33:13 +0100

    Straight to the point

--- examples/aggr.c
+++ examples/aggr.c
@@ -1,69 +1,55 @@
 // This complete example aggregates, in uniquely identified groups, adjacent points in an unbound square grid (the complexity does not depend on the spread of the grid.)
-// - Unbound square grid. Adjacent positions are those that touch horizontally, vertically are diagonally.
-// - Grid filled with a stream, incrementally.
+// - Unbound square grid (Point in pointsInGroups).
+// - Adjacent positions are those that touch horizontally, vertically are diagonally (p_is_adjacent).
+// - Grid filled with a stream, incrementally (add_point).
 // Find groups of adjacent positions.
+// The algorithm comes out to be similar to the union-find algorithm for disjoint-set data structure (https://en.wikipedia.org/wiki/Disjoint-set_data_structure).
 #define _DEFAULT_SOURCE // for random
 #undef NDEBUG           // for assert
 #include <assert.h>
 #include <stdlib.h>
 
-//=========================================================
+//======================== square grid definition =================================
 #include "map.h"
 typedef struct
 {
   long int x, y;
 } Point;
 
+static bool
+p_is_adjacent (Point a, Point b) {
+  static Point adjacent[] = {
+    { 1, 0 },
+    { 1, 1 }, // Diagonally
+  };
+  for (size_t i = 0; i < sizeof (adjacent) / sizeof (*adjacent); i++)
+    if (
+        (a.x == b.x + adjacent[i].x && a.y == b.y + adjacent[i].y)    //
+        || (a.x == b.x - adjacent[i].x && a.y == b.y - adjacent[i].y) // Symetry
+        || (a.x == b.x + adjacent[i].y && a.y == b.y - adjacent[i].x) // Rotation 90°
+        || (a.x == b.x - adjacent[i].y && a.y == b.y + adjacent[i].x) // Rotation 270°
+    )
+      return true;
+
+  return false;
+}
+
 typedef struct
 {
   Point origin, end;
 } Rectangle;
-
-static bool
-r_intersects (Rectangle r1, Rectangle r2) {
-  return !(r2.origin.x > r1.end.x || r2.end.y < r1.origin.y || r1.origin.x > r2.end.x || r1.end.y < r2.origin.y); // ??
-}
-
-static bool
-r_is_contiguous (Rectangle r1, Rectangle r2) {
-  return r_intersects (r1, (Rectangle){ { r2.origin.x - 1, r2.origin.y - 1 }, { r2.end.x + 1, r2.end.y + 1 } });
-}
-
-#define min(a, b) ((a) > (b) ? (b) : (a))
-#define max(a, b) ((a) < (b) ? (b) : (a))
-static Rectangle
-r_union (Rectangle r1, Rectangle r2) {
-  return (Rectangle){
-    (Point){ min (r1.origin.x, r2.origin.x), min (r1.origin.y, r2.origin.y) },
-    (Point){ max (r1.end.x, r2.end.x), max (r1.end.y, r2.end.y) }
-  };
-}
-
-static bool
-r_is_inside (Rectangle r1, Rectangle r2) { // Is r1 inside r2 ?
-  return r1.origin.x >= r2.origin.x && r1.end.x <= r2.end.x && r1.origin.y >= r2.origin.y && r1.end.y <= r2.end.y;
-}
-
-static bool
-r_complements (Rectangle r1, Rectangle r2) {                                                    // Is the union of r1 and r2 a rectangle ?
-  return (r1.origin.y == r2.origin.y && r1.end.y == r2.end.y && r1.end.x + 1 == r2.origin.x)    /* r1 to the left of r2 */
-         || (r1.origin.y == r2.origin.y && r1.end.y == r2.end.y && r1.origin.x == r2.end.x + 1) /* to the right */
-         || (r1.origin.x == r2.origin.x && r1.end.x == r2.end.x && r1.end.y + 1 == r2.origin.y) /* above */
-         || (r1.origin.x == r2.origin.x && r1.end.x == r2.end.x && r1.origin.y == r2.end.y + 1) /* below */;
-}
-
-//=========================================================
+//========================= find groups ================================
 typedef struct
 {
-  Rectangle r;
+  Point p;
   size_t group;
   map *owner;
   bool to_be_removed;
-} RectangleInGroup;
+} PointInGroup;
 
 static const void *
 g_key (void *data) {
-  return &(((const RectangleInGroup *)data)->group);
+  return &(((const PointInGroup *)data)->group);
 }
 
 static int
@@ -77,29 +63,22 @@ g_comparator (const void *key_a, const void *key_b, void *arg) {
 }
 
 static int
-includes_r (const void *data, void *sel_arg) {
-  const RectangleInGroup *rg = data;
-  Rectangle *r = sel_arg;
-  return !rg->to_be_removed && r_is_inside (*r, rg->r);
+equals_p (const void *data, void *sel_arg) {
+  const PointInGroup *rg = data;
+  Point *p = sel_arg;
+  return !rg->to_be_removed && rg->p.x == p->x && rg->p.y == p->y;
 }
 
 static int
-matches_r (const void *data, void *sel_arg) {
-  const RectangleInGroup *rg = data;
-  Rectangle *r = sel_arg;
-  return !rg->to_be_removed && r_complements (rg->r, *r);
-}
-
-static int
-touches_r (const void *data, void *sel_arg) {
-  const RectangleInGroup *rg = data;
-  Rectangle *r = sel_arg;
-  return !rg->to_be_removed && r_is_contiguous (rg->r, *r);
+touches_p (const void *data, void *sel_arg) {
+  const PointInGroup *rg = data;
+  Point *p = sel_arg;
+  return !rg->to_be_removed && p_is_adjacent (rg->p, *p);
 }
 
 static int
 to_be_removed (const void *data, void *sel_arg) {
-  const RectangleInGroup *rg = data;
+  const PointInGroup *rg = data;
   (void)sel_arg;
   return rg->to_be_removed;
 }
@@ -112,7 +91,7 @@ not_to_be_removed (const void *data, void *sel_arg) {
 static int
 onlyonce_ptr_g (void *data, void *op_arg, int *remove) {
   (void)remove;
-  RectangleInGroup *rg = data;
+  PointInGroup *rg = data;
   size_t **group = (size_t **)op_arg;
   assert (group);
   if (!*group)
@@ -126,11 +105,11 @@ onlyonce_ptr_g (void *data, void *op_arg, int *remove) {
 
 static int
 changegroup_g (void *data, void *new_group, int *remove) {
-  RectangleInGroup *rg = data;
+  PointInGroup *rg = data;
   assert (rg->group != *(size_t *)new_group);
   // A key MUST not be changed in place:
   // - A new rectangle is created in the new group.
-  RectangleInGroup *new_r = malloc (sizeof (*new_r));
+  PointInGroup *new_r = malloc (sizeof (*new_r));
   assert (new_r);
   *new_r = *rg;
   new_r->group = *(size_t *)new_group;
@@ -144,7 +123,7 @@ changegroup_g (void *data, void *new_group, int *remove) {
 static int
 regroup_g (void *data, void *op_arg, int *remove) {
   (void)remove;
-  RectangleInGroup *rg = data;
+  PointInGroup *rg = data;
   size_t *ptr_g = op_arg;
   assert (ptr_g);
   //  All rectangles of group rg->group have to switch to group r->group:
@@ -156,66 +135,60 @@ regroup_g (void *data, void *op_arg, int *remove) {
   return 1;
 }
 
-static int
-bbox_g (void *data, void *op_arg, int *remove) {
-  (void)remove;
-  RectangleInGroup *rg = data;
-  Rectangle *bbox = op_arg;
-  *bbox = r_union (rg->r, *bbox);
-  return 1;
-}
-
 static void
-r_add (map *owner, Rectangle r) {
+add_point (map *owner, Point p) {
   static size_t group = 0;
   static int CHECK_DUPLICATES = 1;
 
-  if (CHECK_DUPLICATES && map_traverse (owner, MAP_EXISTS_ONE, 0, includes_r, &r))
+  if (CHECK_DUPLICATES && map_traverse (owner, MAP_EXISTS_ONE, 0, equals_p, &p))
     return;
-
-  RectangleInGroup *rg = 0;
-  if (map_traverse (owner, MAP_REMOVE_ONE, &rg, matches_r, &r) && rg) {
-    r_add (owner, r_union (r, rg->r));
-    free (rg);
-    return;
-  }
 
   size_t *ptr_g = 0;
   size_t g;
-  if (!map_traverse (owner, onlyonce_ptr_g, &ptr_g, touches_r, &r))
+  if (!map_traverse (owner, onlyonce_ptr_g, &ptr_g, touches_p, &p))
     g = ++group;
   else if (ptr_g)
     g = *ptr_g;
   else {
     g = ++group;
-    map_traverse (owner, regroup_g, &g, touches_r, &r);
+    map_traverse (owner, regroup_g, &g, touches_p, &p);
     map_traverse (owner, MAP_REMOVE_ALL, free, to_be_removed, 0);
   }
 
-  RectangleInGroup *p = malloc (sizeof (*p));
-  assert (p);
-  *p = (RectangleInGroup){ .r = r, .group = g, .to_be_removed = false, .owner = owner };
-  assert (map_insert_data (owner, p));
+  PointInGroup *pg = malloc (sizeof (*pg));
+  assert (pg);
+  *pg = (PointInGroup){ .p = p, .group = g, .to_be_removed = false, .owner = owner };
+  assert (map_insert_data (owner, pg));
 }
 
-//=========================================================
-static int
-show_rectangle (void *data, void *op_arg, int *remove) {
-  (void)op_arg;
+//========================= display groups ================================
+[[maybe_unused]] static int
+bbox_r (void *data, void *op_arg, int *remove) {
   (void)remove;
-  RectangleInGroup *r = data;
-  printf ("%zu: {%li, %li} -- {%li, %li}\n", r->group, r->r.origin.x, r->r.origin.y, r->r.end.x, r->r.end.y);
+  PointInGroup *rg = data;
+  Point p = rg->p;
+  Rectangle *bbox = op_arg;
+  if (p.x < bbox->origin.x)
+    bbox->origin.x = p.x;
+  else if (p.x > bbox->end.x)
+    bbox->end.x = p.x;
+  if (p.y < bbox->origin.y)
+    bbox->origin.y = p.y;
+  else if (p.y > bbox->end.y)
+    bbox->end.y = p.y;
   return 1;
 }
 
-[[maybe_unused]] static void
-show_group (const void *key, void *op_arg) {
-  map *owner = op_arg;
-  printf ("%zu: ...\n", *(const size_t *)key);
-  map_find_key (owner, key, show_rectangle, 0, not_to_be_removed, 0);
+[[maybe_unused]] static int
+show_point (void *data, void *op_arg, int *remove) {
+  (void)op_arg;
+  (void)remove;
+  PointInGroup *p = data;
+  printf ("%zu: {%li, %li}\n", p->group, p->p.x, p->p.y);
+  return 1;
 }
 
-static size_t
+[[maybe_unused]] static size_t
 find_or_traverse (map *m, const void *key, map_operator op, void *op_arg, map_selector sel, void *sel_arg) {
   if (key)
     return map_find_key (m, key, op, op_arg, sel, sel_arg);
@@ -223,14 +196,14 @@ find_or_traverse (map *m, const void *key, map_operator op, void *op_arg, map_se
     return map_traverse (m, op, op_arg, sel, sel_arg);
 }
 
-static void
+[[maybe_unused]] static void
 display_group (const void *key, void *op_arg) {
   map *owner = op_arg;
-  RectangleInGroup *rg;
+  PointInGroup *rg;
   if (!find_or_traverse (owner, key, MAP_GET_ONE, &rg, not_to_be_removed, 0) || !rg)
     return;
-  Rectangle bbox = rg->r;
-  find_or_traverse (owner, key, bbox_g, &bbox, not_to_be_removed, 0);
+  Rectangle bbox = (Rectangle){ rg->p, rg->p };
+  find_or_traverse (owner, key, bbox_r, &bbox, not_to_be_removed, 0);
   printf ("(%li,%li)\n", bbox.origin.x, bbox.origin.y);
   for (long int x = bbox.origin.x; x <= bbox.end.x + 2; x++)
     printf ("-");
@@ -238,7 +211,7 @@ display_group (const void *key, void *op_arg) {
   for (long int y = bbox.origin.y; y <= bbox.end.y; y++) {
     printf ("|");
     for (long int x = bbox.origin.x; x <= bbox.end.x; x++)
-      if (find_or_traverse (owner, key, MAP_GET_ONE, &rg, includes_r, &(Rectangle){ { x, y }, { x, y } }))
+      if (find_or_traverse (owner, key, MAP_GET_ONE, &rg, equals_p, &(Point){ x, y }))
         printf ("%c", 'a' + (char)(rg->group % ('z' - 'a' + 1)));
       else
         printf (" ");
@@ -249,24 +222,27 @@ display_group (const void *key, void *op_arg) {
   printf ("\n");
 }
 
-#define NB_ADD 70
-#define NB_LINES 12
-#define NB_COLS 12
+//====================== main ===========================
 int
 main (void) {
-  map *RectanglesInGroups;
-  assert ((RectanglesInGroups = map_create (g_key, g_comparator, 0, 0))); // Dictionary of groups
+  static const size_t NB_POINTS = 70;
+  static const long int NB_LINES = 12;
+  static const long int NB_COLS = 12;
 
-  for (size_t i = 0; i < NB_ADD; i++) {
+  map *pointsInGroups;
+  assert ((pointsInGroups = map_create (g_key, g_comparator, 0, 0))); // Dictionary of disjoint sets (groups)
+
+  for (size_t i = 0; i < NB_POINTS; i++) {
     Point p = { random () % NB_COLS, random () % NB_LINES };
-    r_add (RectanglesInGroups, (Rectangle){ p, p });
+    add_point (pointsInGroups, p); // Add points in disjoint sets of adjacent points.
   }
 
-  // map_traverse (RectanglesInGroups, show_rectangle, 0, not_to_be_removed, 0);
-  display_group (0, RectanglesInGroups);
-  printf ("%zu groups:\n", map_traverse_keys (RectanglesInGroups, 0, 0));
-  map_traverse_keys (RectanglesInGroups, display_group, RectanglesInGroups);
+  // Display results.
+  // map_traverse (RectanglesInGroups, show_point, 0, not_to_be_removed, 0);
+  display_group (0, pointsInGroups);
+  printf ("%zu groups:\n", map_traverse_keys (pointsInGroups, 0, 0));
+  map_traverse_keys (pointsInGroups, display_group, pointsInGroups);
 
-  map_traverse (RectanglesInGroups, MAP_REMOVE_ALL, free, 0, 0);
-  map_destroy (RectanglesInGroups);
+  map_traverse (pointsInGroups, MAP_REMOVE_ALL, free, 0, 0);
+  map_destroy (pointsInGroups);
 }
